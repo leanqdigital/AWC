21cfa573e92122cf000dc9454a8a6da39337e5f6
leanqdigital <117716185+leanqdigital@users.noreply.github.com>
2025-04-08 11:09:36 +0545

    Update unified_chat_student.js

--- unified_chat_student.js
+++ unified_chat_student.js
@@ -729,7 +729,7 @@ const FileUploader = (function () {
     if (!awsParamHash) awsParam = window.awsParam;
     else if (typeof awsParamHash === "string")
       awsParam = encodeAwsParam(awsParamHash);
-    return getS3UploadParams(awsParam, awsParamUrl).then((s3Params) => {
+    return getS3UploadParams("ee037b98f52d6f86c4d3a4cc4522de1e", "https://courses.writerscentre.com.au/s/aws").then((s3Params) => {
       if (!s3Params) {
         const e = new Error("Failed to retrieve S3 upload parameters.");
         e.failures = filesToUpload;
